11a69c5554d9e415247f97b7e0bfbdce52076ff2
amcubk <cubk@vip.qq.com>
2023-09-25 09:48:10 +0800

    HUD ArrayList

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -3,3 +3,5 @@ rootProject.name = "Omni"
 include(":omni-renderer")
 include(":vanilla-1.8.9")
 include(":omni-autowired")
+include("vanilla-1.16.5")
+include("forge-1.8.9")
--- vanilla-1.8.9/src/main/java/client/module/render/HUD.java
+++ vanilla-1.8.9/src/main/java/client/module/render/HUD.java
@@ -2,6 +2,7 @@ package client.module.render;
 
 import client.module.Module;
 import client.module.movement.Sprint;
+import org.union4dev.omni.Omni;
 import org.union4dev.omni.addon.renderer.element.RenderFlag;
 import org.union4dev.omni.addon.renderer.element.impl.LabelElement;
 import org.union4dev.omni.addon.renderer.element.impl.RectangleElement;
@@ -12,15 +13,17 @@ import org.union4dev.omni.annotations.features.RegisterTo;
 import org.union4dev.omni.annotations.features.Startup;
 import org.union4dev.omni.components.features.Category;
 import org.union4dev.omni.components.features.ModuleManager;
+import org.union4dev.omni.elements.module.AbstractModule;
 
 import java.awt.*;
+import java.util.ArrayList;
+import java.util.Comparator;
 
 @RegisterTo(ModuleManager.class)
 @Startup
 public class HUD extends Module {
 
-    private final LabelElement label = new LabelElement(mc.fontRendererObj, "Fuck", 2, 2, Color.WHITE, RenderFlag.STRING_SHADOW);
-    private final RectangleElement rect = new RectangleElement(0, 0, 100, 100, new Color(0, 0, 0, 100), RenderFlag.TRANSPARENT);
+    private final LabelElement label = new LabelElement(mc.fontRendererObj, "Sprinting", 2, 2, Color.WHITE, RenderFlag.STRING_SHADOW);
 
     public HUD() {
         super("HUD", Category.RENDER);
@@ -30,6 +33,23 @@ public class HUD extends Module {
     @ModuleDepend(Sprint.class)
     public void onRender(Render2DEvent event) {
         label.draw(event.getScaledResolution());
-        rect.draw(event.getScaledResolution());
+    }
+
+    @EventTarget
+    public void onRenderArray(Render2DEvent event) {
+        int width = event.getScaledResolution().getScaledWidth();
+        int y = 1;
+        ArrayList<AbstractModule> enabledModules = new ArrayList<>();
+        for (AbstractModule m : Omni.getInstance().getModuleManager().getValues()) {
+            if (m.isEnabled()) {
+                enabledModules.add(m);
+            }
+        }
+        enabledModules.sort((o1, o2) -> mc.fontRendererObj.getStringWidth(o2.getName()) - mc.fontRendererObj.getStringWidth(o1.getName()));
+        for (AbstractModule m : enabledModules) {
+            int moduleWidth = mc.fontRendererObj.getStringWidth(m.getName());
+            mc.fontRendererObj.drawStringWithShadow(m.getName(), width - moduleWidth - 1, y, -1);
+            y += mc.fontRendererObj.FONT_HEIGHT;
+        }
     }
 }
